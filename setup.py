17b165f35488dc74f15a08f35e4225f60c368bba
dakshit9228 <145247776+dakshit9228@users.noreply.github.com>
2023-11-21 20:00:26 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,13 @@ from setuptools import setup, find_packages
 setup(
     name='mypackage',
     version='0.1',
+    author= 'Daxit Golakiya',
+    
     packages=find_packages(),
-    install_requires=[
-        # List any dependencies your package may have
+    install_requires=[ 
+        "requests>=2.25.1",
+        "pandas>=1.2.0"
+        "beautifulsoup4>=4.9.3"
     ],
-    # Other metadata like author, author_email, description, etc.
+    python_requires="3.6"
 )
