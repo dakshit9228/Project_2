3507849591ea85648bd792c370bc18289c668d65
dakshit9228 <145247776+dakshit9228@users.noreply.github.com>
2023-11-26 09:34:02 -1100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name='mypackage',
     version='0.1',
-    author= 'Daxit Golakiya',
+    author='Daxit Golakiya',
     
     packages=find_packages(),
-    install_requires=[ 
+    install_requires=[
         "requests>=2.25.1",
-        "pandas>=1.2.0"
+        "pandas>=1.2.0",  # Note the comma here
         "beautifulsoup4>=4.9.3"
     ],
     python_requires="3.6"
